77d7132980ea1f27270eb58f2fb295dc648da5b0
Teleportal <joshthegreenberg@gmail.com>
2018-01-18 21:32:00 -0600

    practiced restful routes

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,41 @@
+class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    recipe.save
+    render json: recipe.as_json
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
+    render json: recipe.as_json
+  end
+
+  def update
+    recipe = Recipe.find(params[:id])
+
+    recipe.title = params[:title] || recipe.title
+    recipe.chef = params[:chef] || recipe.chef
+    recipe.ingredients = params[:ingredients] || recipe.ingredients
+    recipe.directions = params[:directions] || recipe.directions
+    recipe.save
+
+    render json: recipe.as_json
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    render json: {message: "Successfully destroyed Recipe ##{recipe.id}."}
+  end
+
+end
