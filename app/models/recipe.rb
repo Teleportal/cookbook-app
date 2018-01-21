15f15295a3257359fe95496d72f183b953d4f622
Teleportal <joshthegreenberg@gmail.com>
2018-01-21 17:07:32 -0600

    added model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,40 @@
 class Recipe < ApplicationRecord
+  def friendly_prep_time
+    # input: 125
+    # output: "2 Hours, 5 minutes"
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    time_string = ""
+
+    time_string += "#{hours} #{'Hour'.pluralize(hours)}" if hours > 0
+    time_string += ", " if hours > 0 && minutes > 0
+    time_string += "#{minutes} #{'Minute'.pluralize(minutes)}" if minutes > 0
+      
+
+
+    time_string
+  end
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
+
+  def as_json
+    {
+    title: title,
+    chef: chef,
+    ingredients: ingredients_list,
+    directions: directions_list,
+    created_at: friendly_created_at,
+    prep_time: friendly_prep_time
+    }
+  end
 end
